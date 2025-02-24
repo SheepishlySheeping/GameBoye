dae33d40637f4895a3c322c3efc294a7515f26e5
White Sheep <sheepishlysheeping@gmail.com>
2025-02-24 16:43:29 +0700

    Tictactoe progress

--- src/components/ScreenTicTacToe.js
+++ src/components/ScreenTicTacToe.js
@@ -4,24 +4,24 @@ import { motion } from 'framer-motion';
 import xPNG from '../assets/imgs/xPNG.png';
 import oPNG from '../assets/imgs/oPNG.png';
 
-const name = [xPNG, "i", "c", xPNG, "a", "c", xPNG, "o", "e"];
+const name = ["T", "I", "C", "T", "A", "C", "T", "O", "E"];
 
 const screenTicTacToe = ({ setGameState }) => {
     return (
         <>
             <button onClick={() => setGameState("gameMenu")} className="buttonHover" style={{ position: "absolute", width: "5vw", height: "3vw", top: "5vw", right: "5vw", zIndex: "3" }}>X</button>
-            <div className="menuTitleHolder sway" >
+            <div className="menuTitleHolder sway" style={{ backgroundColor: "red", marginTop: "3%"}} >
                 {name.map((a, index) => (
                                     <motion.div
                                         key={index}
-                                        initial={{y: "20vw"}}
-                                        animate={{y: 0}}
+                                        style={{
+                                            fontSize: "5rem"
+                                        }}
                                     >
-                                        {typeof a === "string" ? a : <img src={a} alt="X" style={{ height: "1.5em" }} />}
+                                     {a}
                                     </motion.div>
                                 ))}
             </div>
-            <img src={xPNG} style={{ position: "absolute", bottom: 0, width: "20%", height: "20%"}} />
         </>
     )
 }
