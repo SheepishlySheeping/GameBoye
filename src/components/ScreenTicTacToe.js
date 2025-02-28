b40ee0fc5bb2aa6fbc1d73420c40454d6288bda5
White Sheep <sheepishlysheeping@gmail.com>
2025-02-28 16:32:53 +0700

    Tictactoe menu implementation

--- src/components/ScreenTicTacToe.js
+++ src/components/ScreenTicTacToe.js
@@ -6,44 +6,50 @@ import oPNG from '../assets/imgs/oPNG.png';
 
 const name = ["T", "I", "C", "T", "A", "C", "T", "O", "E"];
 
-const screenTicTacToe = ({ setGameState }) => {
+const ScreenTicTacToe = ({ setGameState }) => {
+    const [boardSize, setBoardSize] = useState(null);
+    const [difficulty, setDifficulty] = useState(null);
+
+    const handleClick = (mode) => {
+        if (mode == null) {
+            setBoardSize(mode);
+            setDifficulty(mode);
+        }
+        else {
+            if (boardSize == null) setBoardSize(mode);
+            else setDifficulty(mode);
+        }
+    }
+
     return (
         <>
             <button onClick={() => setGameState("gameMenu")} className="buttonHover" style={{ position: "absolute", width: "5vw", height: "3vw", top: "5vw", right: "5vw", zIndex: "3" }}>X</button>
-            <div className="menuTitleHolder sway" style={{ backgroundColor: "red", marginTop: "3%", width: "100%"}} >
+            <div className="menuTitleHolder sway" style={{ backgroundColor: "red", marginTop: "3%", width: "100%" }} >
                 {name.map((a, index) => (
-                                    <motion.div
-                                        key={index}
-                                        style={{
-                                            fontSize: "5rem"
-                                        }}
-                                    >
-                                     {a}
-                                    </motion.div>
-                                ))}
+                    <motion.div
+                        key={index}
+                        style={{
+                            fontSize: "5rem"
+                        }}
+                    >
+                        {a}
+                    </motion.div>
+                ))}
             </div>
-            <div style={{backgroundColor: "blue", width: "100%", height: "30%", marginTop: "15%", position: "relative"}}>
-                <button>
-                    one
+            <motion.div animate={{x : boardSize == null ? [0, 100] : [-100, 0]}} transition={{ duration: 1.5, ease: 'easeInOut' }} className="sway" style={{ backgroundColor: "blue", width: "100%", height: "35%", marginTop: "15%", position: "relative", display: "flex", flexWrap: "wrap" }}>
+                <button onClick={() => handleClick(boardSize == null ? "3x3" : "PVP")} style={{ width: "23.33%", height: "68%", margin: "1% 5%", backgroundColor: "orange" }}>
+                    {boardSize == null ? "3x3" : "PVP"}
                 </button>
-                <button>
-                    two
+                <button onClick={() => handleClick(boardSize == null ? "6x6" : "Normal")} style={{ width: "23.33%", height: "68%", margin: "1% 5%", backgroundColor: "purple" }}>
+                    {boardSize == null ? "6x6" : "Normal"}
                 </button>
-                <button>
-                    three
+                <button onClick={() => handleClick(boardSize == null ? "9x9" : "Hard")} style={{ width: "23.33%", height: "68%", margin: "1% 5%", backgroundColor: "orange" }}>
+                    {boardSize == null ? "9x9" : "Hard"}
                 </button>
-                <button>
-                    four
-                </button>
-                <button>
-                    five
-                </button>
-                <button>
-                    six
-                </button>
-            </div>
+                {boardSize !== null && <button onClick={() => handleClick(null)} style={{ height: "30%", width: "20%", backgroundColor: "peachpuff", opacity: "50%" }}>Back</button>}
+            </motion.div>
         </>
     )
 }
 
-export default screenTicTacToe;
+export default ScreenTicTacToe;
